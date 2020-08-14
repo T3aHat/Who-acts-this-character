2711f6e7d33221291b4475d1029d69943d3e95a7
T3aHat <secwhiteahat@gmail.com>
2020-08-15 03:00:10 +0900

    fix bug

--- cv.js
+++ cv.js
@@ -21,39 +21,48 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
+function get_resp(res) {
+  resp = res.split(
+    /<span class="mw-headline" id="登場人物">登場人物<\/span>/
+  )[1];
+  if (resp !== undefined) {
+    return resp;
+  } else if (resp === undefined) {
+    resp = res.split(
+      '<span class="mw-headline" id="登場キャラクター">登場キャラクター</span>'
+    )[1];
+  }
+  if (resp !== undefined) {
+    return resp;
+  } else if (resp === undefined) {
+    resp = res.split(
+      /<span class="mw-headline" id="[^"]*登場人物[^"]*">[^<]*登場人物[^<]*<\/span>/
+    )[1];
+  }
+  if (resp !== undefined) {
+    return resp;
+  } else {
+    return;
+  }
+}
 parent = document.getElementById("firstHeading");
 divv = document.createElement("div");
 divv.classList.add("cv");
 res = document.getElementById("mw-content-text").innerHTML;
-resp = res.split(/<span class="mw-headline" id="登場人物">登場人物<\/span>/)[1];
-if (resp === undefined) {
-  resp = res.split(
-    '<span class="mw-headline" id="登場キャラクター">登場キャラクター</span>'
-  )[1];
-}
-if (resp === undefined) {
-  resp = res.split(
-    /<span class="mw-headline" id="[^"]*登場人物">[^<]*登場人物<\/span>/
-  )[1];
-}
+resp = get_resp(res);
 resp = resp.split("</h2>")[1];
 resp = resp.split("<h2>")[0].replace(/\n/g, "");
-dtdd = resp.match(/<dt.*?\/dd>/g);
-dt = resp.match(/<dt>.*?<\/dt>/g);
+dtdd = resp.match(/<dt((?!<dt).)*?\/dd>/g);
 const parser = new DOMParser();
-charlist = [];
-for (let i = 0; i < dt.length; i++) {
-  doc = parser.parseFromString(dt[i], "text/html");
-  char = doc.getElementsByTagName("dt")[0].textContent.replace(/\[.*?\]/g, "");
-  charlist.push(char);
-}
 for (let i = 0; i < dtdd.length; i++) {
+  doc = parser.parseFromString(dtdd[i], "text/html");
+  char = doc.getElementsByTagName("dt")[0].textContent.replace(/\[.*?\]/g, "");
   dddd = dtdd[i].match(/声.*?[：:-].*?<\/dd>/g);
   try {
     divv.insertAdjacentHTML(
       "beforeend",
       "<strong>" +
-        charlist[i] +
+        char +
         "</strong>" +
         " : " +
         dddd[0]
